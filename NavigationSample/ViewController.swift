382bc16b1f295c48a823a51543a250611ec91afd
Tetsuro Nakamura <t2low.n@gmail.com>
2023-04-06 00:14:11 +0900

    Initial Commit

---
+++ NavigationSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NavigationSample
+//
+//  Created by Tetsuro Nakamura on 2023/04/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
